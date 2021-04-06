f88cc346e4f9fd1c55fd6ff4ae8ee96194ad61ee
Zedd0202 <ch41150@hanmail.net>
2021-04-06 15:32:43 +0900

    Diffable DataSource예제

--- DiffableDataSource_Example/ViewController.swift
+++ DiffableDataSource_Example/ViewController.swift
@@ -6,14 +6,137 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    
+    enum Section: CaseIterable {
+        case main
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var arr = ["Zedd", "Alan Walker", "David Guetta", "Avicii", "Marshmello", "Steve Aoki", "R3HAB", "Armin van Buuren", "Skrillex", "Illenium", "The Chainsmokers", "Don Diablo", "Afrojack", "Tiesto", "KSHMR", "DJ Snake", "Kygo", "Galantis", "Major Lazer", "Vicetone"
+    ]
+    
+    var filteredArr: [String] = []
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, String>!
+    
+    var isFiltering: Bool {
+        let searchController = self.navigationItem.searchController
+        let isActive = searchController?.isActive ?? false
+        let isSearchBarHasText = searchController?.searchBar.text?.isEmpty == false
+        return isActive && isSearchBarHasText
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.collectionView.collectionViewLayout = self.createLayout()
+        self.setupSearchController()
+        self.setupDataSource()
+        self.performQuery(with: nil)
+    }
+    
+    func setupSearchController() {
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.searchController = searchController
+        self.navigationItem.searchController?.searchResultsUpdater = self
+        self.navigationItem.title = "Search DJ"
+        self.navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    func setupDataSource() {
+        // MARK: - Way 1
+        self.collectionView.register(DJCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        self.dataSource =
+            UICollectionViewDiffableDataSource<Section, String>(collectionView: self.collectionView) { (collectionView, indexPath, dj) -> UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? DJCollectionViewCell else { preconditionFailure() }
+            cell.configure(text: dj)
+            return cell
+        }
+        
+        // MARK: - Way 2
+        /*
+        let cellRegistration = UICollectionView.CellRegistration
+        <DJCollectionViewCell, String> { (cell, indexPath, dj) in
+            cell.configure(text: dj)
+        }
+        
+        self.dataSource = UICollectionViewDiffableDataSource<Section, String>(collectionView: self.collectionView) {
+            (collectionView, indexPath, dj) -> UICollectionViewCell? in
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: dj)
+        }
+        */
     }
 
+    func performQuery(with filter: String?) {
+        let filtered = self.arr.filter { $0.hasPrefix(filter ?? "") }
 
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(filtered)
+        self.dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection in
+            let contentSize = layoutEnvironment.container.effectiveContentSize
+            let columns = contentSize.width > 800 ? 3 : 2
+            let spacing = CGFloat(10)
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(1.0))
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .absolute(32))
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: columns)
+            group.interItemSpacing = .fixed(spacing)
+
+            let section = NSCollectionLayoutSection(group: group)
+            section.interGroupSpacing = spacing
+            section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+            return section
+        }
+        return layout
+    }
 }
 
+extension ViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let text = searchController.searchBar.text
+        self.performQuery(with: text)
+    }
+}
+
+class DJCollectionViewCell: UICollectionViewCell {
+    
+    weak var label: UILabel?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        self.contentView.backgroundColor = .lightGray
+        self.contentView.layer.borderWidth = 0.5
+        self.contentView.layer.borderColor = UIColor.black.cgColor
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.frame = self.contentView.frame
+        self.contentView.addSubview(label)
+        self.label = label
+    }
+    
+    func configure(text: String) {
+        self.label?.text = text
+    }
+}
