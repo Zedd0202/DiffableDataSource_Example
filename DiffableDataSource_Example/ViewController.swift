a253bd9805da5a1d52855dd919df0fccaad8ed73
Zedd0202 <ch41150@hanmail.net>
2021-04-12 17:19:02 +0900

    필요없는 프로퍼티 제거

--- DiffableDataSource_Example/ViewController.swift
+++ DiffableDataSource_Example/ViewController.swift
@@ -18,9 +18,7 @@ class ViewController: UIViewController {
     
     var arr = ["Zedd", "Alan Walker", "David Guetta", "Avicii", "Marshmello", "Steve Aoki", "R3HAB", "Armin van Buuren", "Skrillex", "Illenium", "The Chainsmokers", "Don Diablo", "Afrojack", "Tiesto", "KSHMR", "DJ Snake", "Kygo", "Galantis", "Major Lazer", "Vicetone"
     ]
-    
-    var filteredArr: [String] = []
-    
+        
     var dataSource: UICollectionViewDiffableDataSource<Section, String>!
     
     var isFiltering: Bool {
